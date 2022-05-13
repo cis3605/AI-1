b1d20e1562e3c40d5b8b372f956d14385d057230
cis3605 <61548104+cis3605@users.noreply.github.com>
2022-05-13 15:14:47 +0900

    Add files via upload

---
+++ 2.numpy.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#arr1 = np.random.randn(5, 3)  # 가우스 난수
+#print(arr1)
+print('---------------')
+#print(np.abs(arr1))
+#print(np.sqrt(arr1))
+#print(np.square(arr1))
+#print(np.sign(arr1))
+#print(np.ceil(arr1))
+#print(np.floor(arr1))
+#arr2=np.sqrt(arr1)
+#print(arr2)
+#print(np.isnan(arr2))
+#print(np.sort(arr1, axis=0))
+
+#arr1 = np.array([5,6,4,8,2,9])
+#print(np.sort(arr1)[::-1])
+
+arr1=np.array([[1,2,3],[3,4,5]])
+arr2=np.array([[7,8],[9,10],[1,2]])
+#print(np.multiply(arr1,arr2))
+#print(np.matmul(arr1,arr2))
+print(arr1)
+print(arr1 *2)
